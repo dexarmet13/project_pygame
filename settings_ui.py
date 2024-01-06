1e034a53d21c44e78f16f49771f0dabcec74b34c
Dedstate <Ted.tukan@gmail.com>
2024-01-06 10:22:11 +0300

    Update settings_ui.py

--- settings_ui.py
+++ settings_ui.py
@@ -27,84 +27,84 @@ class SettingsUI(QWidget):
 
         self.main_layout = QGridLayout(self)
 
-        self.back_button = QPushButton("Назад")
-        self.back_button.setStyleSheet("color: black;")
-        self.back_button.setFont(self._font)
-        self.main_layout.addWidget(self.back_button, 0, 0, 1, 3)
-
         label = QLabel("Сложность")
         self.set_lables_style(label)
-        self.main_layout.addWidget(label, 2, 0, 1, 1)
+        self.main_layout.addWidget(label, 1, 0, 1, 1)
 
         self.dificulty_combo_box = QComboBox()
         self.dificulty_combo_box.addItems(["Легкий", "Средний", "Тяжкий"])
-        self.main_layout.addWidget(self.dificulty_combo_box, 2, 1, 1, 2)
+        self.main_layout.addWidget(self.dificulty_combo_box, 1, 1, 1, 2)
 
         label = QLabel("Разрешение")
         self.set_lables_style(label)
-        self.main_layout.addWidget(label, 3, 0, 1, 1)
+        self.main_layout.addWidget(label, 2, 0, 1, 1)
 
         self.game_resolution_combo_box = QComboBox()
         self.game_resolution_combo_box.addItems([
             "2560*1440", "1920*1080", "1280*720"
         ])
-        self.main_layout.addWidget(self.game_resolution_combo_box, 3, 1, 1, 2)
+        self.main_layout.addWidget(self.game_resolution_combo_box, 2, 1, 1, 2)
 
         label = QLabel("Качество")
         self.set_lables_style(label)
-        self.main_layout.addWidget(label, 4, 0, 1, 1)
+        self.main_layout.addWidget(label, 3, 0, 1, 1)
 
         self.quality_level_combo_box = QComboBox()
         self.quality_level_combo_box.addItems(["Низкое", "Среднее", "Высокое"])
-        self.main_layout.addWidget(self.quality_level_combo_box, 4, 1, 1, 2)
+        self.main_layout.addWidget(self.quality_level_combo_box, 3, 1, 1, 2)
 
         label = QLabel("Режим")
         self.set_lables_style(label)
-        self.main_layout.addWidget(label, 5, 0, 1, 1)
+        self.main_layout.addWidget(label, 4, 0, 1, 1)
 
         for i, button in enumerate(["Оконный", "Полноэкранный"]):
             button = QPushButton(f"{button}")
-            button.setStyleSheet("color: black;")
-            button.setFont(self._font)
-            self.main_layout.addWidget(button, 5, i + 1, 1, 1)
+            self.set_button_stylesheet(button)
+            self.main_layout.addWidget(button, 4, i + 1, 1, 1)
 
         label = QLabel("Ограничение FPS")
         self.set_lables_style(label)
-        self.main_layout.addWidget(label, 6, 0, 1, 1)
+        self.main_layout.addWidget(label, 5, 0, 1, 1)
 
         self.fps_limit_combo_box = QComboBox()
         self.fps_limit_combo_box.addItems([
             "Без ограничений", "Ограничение 60", "Ограничение 30"
         ])
-        self.main_layout.addWidget(self.fps_limit_combo_box, 6, 1, 1, 2)
+        self.main_layout.addWidget(self.fps_limit_combo_box, 5, 1, 1, 2)
 
         label = QLabel("Верт")
         self.set_lables_style(label)
-        self.main_layout.addWidget(label, 7, 0, 1, 1)
+        self.main_layout.addWidget(label, 6, 0, 1, 1)
 
         for i, button in enumerate(["Вкл", "Выкл"]):
             button = QPushButton(f"{button}")
-            button.setStyleSheet("color: black;")
-            button.setFont(self._font)
-            self.main_layout.addWidget(button, 7, i + 1, 1, 1)
+            self.set_button_stylesheet(button)
+            self.main_layout.addWidget(button, 6, i + 1, 1, 1)
+
+        self.back_button = QPushButton("Назад")
+        self.set_button_stylesheet(self.back_button)
+        self.main_layout.addWidget(self.back_button, 7, 0, 1, 1)
 
         self.accept_button = QPushButton("Применить")
-        self.accept_button.setStyleSheet("color: black;")
-        self.accept_button.setFont(self._font)
-        self.main_layout.addWidget(self.accept_button, 8, 0, 1, 3)
+        self.set_button_stylesheet(self.accept_button)
+        self.main_layout.addWidget(self.accept_button, 7, 1, 1, 1)
+
+        self.reset = QPushButton("Сбросить")
+        self.set_button_stylesheet(self.reset)
+        self.main_layout.addWidget(self.reset, 7, 2, 1, 1)
 
         for i in range(self.main_layout.columnCount()):
-            self.main_layout.setColumnStretch(i, 1)
+            self.main_layout.setColumnStretch(i, 0)
 
         self.volume_label = QLabel("Громкость")
         self.set_lables_style(self.volume_label)
-        self.main_layout.addWidget(self.volume_label, 1, 0, 1, 1)
+        self.main_layout.addWidget(self.volume_label, 0, 0, 1, 1)
 
         self.volume_slider = QSlider(Qt.Horizontal)
         self.volume_slider.setMinimum(0)
         self.volume_slider.setMaximum(100)
         self.volume_slider.setValue(50)
-        self.main_layout.addWidget(self.volume_slider, 1, 1, 1, 2)
+        self.main_layout.addWidget(self.volume_slider, 0, 1, 1, 2)
 
         self.frame.setLayout(self.main_layout)
 
@@ -118,7 +118,7 @@ class SettingsUI(QWidget):
         temp_font = QFont(font_name)
         temp_label.setFont(temp_font)
 
-        font_info = QFontInfo(temp_label.font())
+        font_info = QFontInfo(temp_label._font())
 
         if font_info.family() == temp_font.family():
             self._font = temp_font
@@ -133,4 +133,8 @@ class SettingsUI(QWidget):
     def set_lables_style(self, label):
         label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         label.setStyleSheet("color: white;")
-        label.setFont(self.font)
+        label.setFont(self._font)
+
+    def set_button_stylesheet(self, button):
+        button.setStyleSheet("color: black;")
+        button.setFont(self._font)
