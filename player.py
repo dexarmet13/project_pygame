bea3afb63a82d83fac08220e8421e3bf075d9f0e
dexarmet13 <bletbune355@gmail.com>
2024-01-08 14:23:55 +0300

    Добавлена анимация игрока и ее функции (без кадров)

--- player.py
+++ player.py
@@ -1,111 +1,132 @@
 import pygame
+from const import *
+import pyganim
 
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 
 class Player(pygame.sprite.Sprite):
-    # Изначально игрок смотрит вправо, поэтому эта переменная True
-    right = True
-
-    # Методы
-    def __init__(self):
-        # Стандартный конструктор класса
-        # Нужно ещё вызывать конструктор родительского класса
+    def __init__(self, image_path="src/hero_texture.png"):
         super().__init__()
 
-        # Создаем изображение для игрока
-        # Изображение находится в этой же папке проекта
-        self.image = pygame.image.load('x1589896344.png.pagespeed.ic.Obt0dMzh8-.png')
-
-        # Установите ссылку на изображение прямоугольника
+        self.image = pygame.image.load(image_path)
         self.rect = self.image.get_rect()
-
-        # Задаем вектор скорости игрока
         self.change_x = 0
         self.change_y = 0
+        self.level = None
+        self.facing_right = True
+
+        # delayAnim= []
+        # for anim in ANIMATION_RIGHT:
+        #     delayAnim.append((anim, ANIMATION_FPS))
+        # self.delayAnimRight = pyganim.PygAnimation(delayAnim)
+        # self.delayAnimRight.play()
+        # delayAnim = []
+        # for anim in ANIMATION_LEFT:
+        #     delayAnim.append((anim, ANIMATION_FPS))
+        # self.delayAnimLeft = pyganim.PygAnimation(delayAnim)
+        # self.delayAnimLeft.play()
+        #
+        # self.delayAnimStay = pyganim.PygAnimation(ANIMATION_STAY)
+        # self.delayAnimStay.play()
+        # self.delayAnimStay.blit(self.image, (0, 0))
+        #
+        # self.boltAnimJumpLeft = pyganim.PygAnimation(ANIMATION_JUMP_LEFT)
+        # self.boltAnimJumpLeft.play()
+        #
+        # self.boltAnimJumpRight = pyganim.PygAnimation(ANIMATION_JUMP_RIGHT)
+        # self.boltAnimJumpRight.play()
+        #
+        # self.boltAnimJump = pyganim.PygAnimation(ANIMATION_JUMP)
+        # self.boltAnimJump.play()
 
     def update(self):
-        # В этой функции мы передвигаем игрока
-        # Сперва устанавливаем для него гравитацию
-        self.calc_grav()
+        self._calc_grav()
 
-        # Передвигаем его на право/лево
-        # change_x будет меняться позже при нажатии на стрелочки клавиатуры
         self.rect.x += self.change_x
+        self._handle_horizontal_collisions()
 
-        # Следим ударяем ли мы какой-то другой объект, платформы, например
-        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
-        # Перебираем все возможные объекты, с которыми могли бы столкнуться
+        self.rect.y += self.change_y
+        self._handle_vertical_collisions()
+
+    def _calc_grav(self):
+        if self.change_y == 0:
+            self.change_y = 1
+        else:
+            self.change_y += GRAVITY
+
+        if self.rect.bottom >= SCREEN_HEIGHT and self.change_y >= 0:
+            self.change_y = 0
+            self.rect.bottom = SCREEN_HEIGHT
+
+    def _handle_horizontal_collisions(self):
+        block_hit_list = pygame.sprite.spritecollide(
+            self, self.level.platform_list, False
+        )
         for block in block_hit_list:
-            # Если мы идем направо,
-            # устанавливает нашу правую сторону на левой стороне предмета, которого мы ударили
             if self.change_x > 0:
                 self.rect.right = block.rect.left
             elif self.change_x < 0:
-                # В противном случае, если мы движемся влево, то делаем наоборот
                 self.rect.left = block.rect.right
 
-        # Передвигаемся вверх/вниз
-        self.rect.y += self.change_y
-
-        # То же самое, вот только уже для вверх/вниз
-        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+    def _handle_vertical_collisions(self):
+        block_hit_list = pygame.sprite.spritecollide(
+            self, self.level.platform_list, False
+        )
         for block in block_hit_list:
-            # Устанавливаем нашу позицию на основе верхней / нижней части объекта, на который мы попали
             if self.change_y > 0:
                 self.rect.bottom = block.rect.top
             elif self.change_y < 0:
                 self.rect.top = block.rect.bottom
 
-            # Останавливаем вертикальное движение
             self.change_y = 0
-
-    def calc_grav(self):
-        # Здесь мы вычисляем как быстро объект будет
-        # падать на землю под действием гравитации
-        if self.change_y == 0:
-            self.change_y = 1
-        else:
-            self.change_y += .95
-
-        # Если уже на земле, то ставим позицию Y как 0
-        if self.rect.y >= SCREEN_HEIGHT - self.rect.height and self.change_y >= 0:
-            self.change_y = 0
-            self.rect.y = SCREEN_HEIGHT - self.rect.height
 
     def jump(self):
-        # Обработка прыжка
-        # Нам нужно проверять здесь, контактируем ли мы с чем-либо
-        # или другими словами, не находимся ли мы в полете.
-        # Для этого опускаемся на 10 единиц, проверем соприкосновение и далее поднимаемся обратно
-        self.rect.y += 10
-        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
-        self.rect.y -= 10
+        self.rect.y += 2
+        platform_hit_list = pygame.sprite.spritecollide(
+            self, self.level.platform_list, False
+        )
+        self.rect.y -= 2
 
-        # Если все в порядке, прыгаем вверх
-        if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
-            self.change_y = -16
+        if platform_hit_list or self.rect.bottom >= SCREEN_HEIGHT:
+            self.change_y = JUMP_STRENGTH
+            # self.move_change_image(False, False, True)
 
-    # Передвижение игрока
     def go_left(self):
-        # Сами функции будут вызваны позже из основного цикла
-        self.change_x = -9  # Двигаем игрока по Х
-        if (self.right):  # Проверяем куда он смотрит и если что, то переворачиваем его
-            self.flip()
-            self.right = False
+        self.change_x = -MOVE_SPEED
+        if self.facing_right:
+            self.move_change_image(False, True)
+            self.facing_right = False
 
     def go_right(self):
-        # то же самое, но вправо
-        self.change_x = 9
-        if (not self.right):
-            self.flip()
-            self.right = True
+        self.change_x = MOVE_SPEED
+        if not self.facing_right:
+            self.move_change_image(True)
+            self.facing_right = True
 
     def stop(self):
-        # вызываем этот метод, когда не нажимаем на клавиши
         self.change_x = 0
+        # self.move_change_image()
 
-    def flip(self):
-        # переворот игрока (зеркальное отражение)
+    def move_change_image(self, right=False, left=False, up=False, stop=False):
         self.image = pygame.transform.flip(self.image, True, False)
+        # if up:
+        #     self.image.fill(pygame.Color(COLOR))
+        #     self.boltAnimJump.blit(self.image, (0, 0))
+        #
+        # if left:
+        #     self.image.fill(pygame.Color(COLOR))
+        #     if up:
+        #         self.boltAnimJumpLeft.blit(self.image, (0, 0))
+        #     else:
+        #         self.delayAnimLeft.blit(self.image, (0, 0))
+        #
+        # if right:
+        #     self.image.fill(pygame.Color(COLOR))
+        #     if up:
+        #         self.boltAnimJumpRight.blit(self.image, (0, 0))
+        #     else:
+        #         self.delayAnimRight.blit(self.image, (0, 0))
+        #
+        # if not (left or right):
+        #     if not up:
+        #         self.image.fill(pygame.Color(COLOR))
+        #         self.boltAnimStay.blit(self.image, (0, 0))
