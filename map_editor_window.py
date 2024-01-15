4654706038d8bfcc941bfa40cc863706852033c0
Dedstate <Ted.tukan@gmail.com>
2024-01-15 20:50:28 +0300

    Update map_editor_window.py

--- map_editor_window.py
+++ map_editor_window.py
@@ -24,7 +24,7 @@ class MapEditorUI:
         self.bg = bg_image
         self.images = images
 
-    def draw(self, screen, highlight_fixed_area=False, fixed_area_rect=None):
+    def draw(self, screen):
         self.editor_surf.fill((255, 255, 255))
         self.editor_surf.blit(self.bg, (0, 0))
 
@@ -71,11 +71,6 @@ class MapEditorUI:
                     ),
                 )
 
-        if highlight_fixed_area and fixed_area_rect is not None:
-            pygame.draw.rect(
-                self.editor_surf, (0, 255, 0), fixed_area_rect, 2
-            )  # added a thickness of 2 for the border
-
         self.editor_surf.blit(text_surf, text_rect)
         screen.blit(self.editor_surf, (0, 0))
 
@@ -86,6 +81,11 @@ class MapEditorWindow:
 
         self._screen_size = screen_size
         self.screen = pygame.display.set_mode(self._screen_size)
+
+        self.cell_width = self._screen_size[0] * 0.85 / 100 * 5
+        self.cell_height = self._screen_size[1] * 0.75 / 100 * 5
+
+        self.selected_cells = []
 
         self._font = pygame.font.Font(None, 36)
 
@@ -137,7 +137,8 @@ class MapEditorWindow:
             self._screen_size[1] * 0.75,
         )
 
-        self.map_editor_ui.draw(self.screen)
+        is_selecting = False
+        selection_start = None
 
         while running:
             events = pygame.event.get()
@@ -146,19 +147,93 @@ class MapEditorWindow:
                 if event.type == pygame.QUIT:
                     running = False
 
-                elif event.type == pygame.KEYDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        is_selecting = True
+                        selection_start = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        is_selecting = False
+                        current_mouse_pos = pygame.mouse.get_pos()
+
+                        if fixed_area.collidepoint(
+                            current_mouse_pos
+                        ) and fixed_area.collidepoint(selection_start):
+                            self.select_cells(
+                                selection_start, current_mouse_pos
+                            )
+
+                if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_F1:
                         self.toggle_soundtrack(flag_soundtrack)
                         flag_soundtrack = not flag_soundtrack
 
-                    if event.key == pygame.K_ESCAPE:
+                    elif event.key == pygame.K_ESCAPE:
                         running = False
 
-            cursor_pos = pygame.mouse.get_pos()
-            highlight_area = fixed_area.collidepoint(cursor_pos)
+            self.map_editor_ui.draw(self.screen)
 
-            self.map_editor_ui.draw(self.screen, highlight_area, fixed_area)
+            self.redraw_selected_cells()
+
+            if is_selecting:
+                current_mouse_pos = pygame.mouse.get_pos()
+                if fixed_area.collidepoint(
+                    current_mouse_pos
+                ) and fixed_area.collidepoint(selection_start):
+                    self.highlight_selection_area(
+                        selection_start, current_mouse_pos
+                    )
 
             pygame.display.flip()
 
         pygame.quit()
+
+    def grid_position(self, screen_position):
+        grid_x = int(screen_position[0] // self.cell_width)
+        grid_y = int(screen_position[1] // self.cell_height)
+        return grid_x, grid_y
+
+    def highlight_selection_area(self, start_pos, current_pos):
+        rect = pygame.Rect(
+            min(start_pos[0], current_pos[0]),
+            min(start_pos[1], current_pos[1]),
+            abs(start_pos[0] - current_pos[0]),
+            abs(start_pos[1] - current_pos[1]),
+        )
+
+        pygame.draw.rect(self.screen, (0, 255, 0), rect, 1)
+
+    def select_cells(self, start_pos, end_pos):
+        start_grid_pos = self.grid_position(start_pos)
+        end_grid_pos = self.grid_position(end_pos)
+
+        new_selected_cells = []
+        for x in range(
+            min(start_grid_pos[0], end_grid_pos[0]),
+            max(start_grid_pos[0], end_grid_pos[0]) + 1,
+        ):
+            for y in range(
+                min(start_grid_pos[1], end_grid_pos[1]),
+                max(start_grid_pos[1], end_grid_pos[1]) + 1,
+            ):
+                new_selected_cells.append((x, y))
+
+        self.selected_cells.extend(new_selected_cells)
+
+        for cell in self.selected_cells:
+            self.draw_cell(cell, color=(0, 255, 0))
+
+    def draw_cell(self, cell, color):
+        x, y = cell
+        cell_rect = pygame.Rect(
+            x * self.cell_width + 1,
+            y * self.cell_height,
+            self.cell_width,
+            self.cell_height,
+        )
+        pygame.draw.rect(self.screen, color, cell_rect)
+
+    def redraw_selected_cells(self):
+        for cell in self.selected_cells:
+            self.draw_cell(cell, color=(0, 255, 0))
